c2402c882623405026478aaff7bc713b1d5b2c98
André Godinho <andregodinho06@gmail.com>
2020-04-19 14:24:32 +0100

    Deleted files

--- cnv/prepare_workspace.sh
+++
@@ -1,5 +0,0 @@
-source BIT/java-config-rnl-vm.sh
-java -version
-export CLASSPATH="%CLASSPATH%:~/SudokuCloud/cnv/instrumented:~/SudokuCloud/cnv/project:~/SudokuCloud/cnv/BIT:~/SudokuCloud/cnv/BIT/samples:."
-echo "CLASSPATH defined as: %CLASSPATH%:~/SudokuCloud/cnv/instrumented:~/SudokuCloud/cnv/project:~/SudokuCloud/cnv/BIT:~/SudokuCloud/cnv/BIT/samples:."
-
